52b940b3bd1043a0992468aa99ff7422e3b5f8e8
shrek.wang <devnull@localhost>
2011-06-30 22:27:04 +0800

    change launch connector to attach connector.

--- sztools/java/com/google/code/vimsztool/debug/Debugger.java
+++ sztools/java/com/google/code/vimsztool/debug/Debugger.java
@@ -1,22 +1,36 @@
 package com.google.code.vimsztool.debug;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.google.code.vimsztool.compiler.CompilerContext;
+import com.google.code.vimsztool.compiler.CompilerContextManager;
 import com.google.code.vimsztool.util.VjdeUtil;
+import com.sun.jdi.Bootstrap;
 import com.sun.jdi.IncompatibleThreadStateException;
 import com.sun.jdi.Location;
 import com.sun.jdi.ReferenceType;
 import com.sun.jdi.StackFrame;
 import com.sun.jdi.ThreadReference;
 import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.connect.AttachingConnector;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
 
 public class Debugger {
 
 	private static Debugger instance = new Debugger();
 
 	private VirtualMachine vm = null;
+	private Process process = null;
 	private EventHandler eventHandler = null;
 	private String vimServerName = "";
 	private CompilerContext compilerContext =null;
@@ -29,7 +43,9 @@ public class Debugger {
 	}
 
 	private void startProcess() {
-		Process process = vm.process();
+		if (process == null) {
+			process = vm.process();
+		}
 		BreakpointManager bm = BreakpointManager.getInstance();
 		bm.tryCreatePrepareRequest();
 
@@ -66,7 +82,7 @@ public class Debugger {
         }
 		if (vm != null) 
 			return "VirtualMachine is not null";
-		vm = ConnectorManager.launch(className,classPathXml,opts,args);
+		vm = launch(className,classPathXml,opts,args);
 		startProcess();
 		return "";
 		
@@ -75,7 +91,7 @@ public class Debugger {
 	public String attach(String port) {
 		if (vm != null) 
 			return "VirtualMachine is not null";
-		vm = ConnectorManager.attach(port);
+		vm = attachToVm(port);
 		if (vm == null)
 			return "attach to port fails.";
 		startProcess();
@@ -288,6 +304,138 @@ public class Debugger {
 
 	public CompilerContext getCompilerContext() {
 		return compilerContext;
+	}
+	
+	private String getClassPath(String classPathXml) {
+		CompilerContextManager ccm = CompilerContextManager.getInstnace();
+		CompilerContext ctx = ccm.getCompilerContext(classPathXml);
+
+		List<URL> urls = ctx.getClassPathUrls();
+		StringBuilder sb = new StringBuilder();
+		for (URL url : urls) {
+			sb.append(url.getPath()).append(File.pathSeparator);
+		}
+		return sb.toString();
+	}
+	
+	public int getAvailPort() {
+		int port = 0;
+		try {
+			ServerSocket socket = new ServerSocket(0);
+			port = socket.getLocalPort();
+			socket.close();
+		} catch (Exception e) {
+		}
+		return port;
+	}
+	
+	public VirtualMachine launch(String mainClass, String classPathXml,
+			List<String> opts,List<String> args) {
+		
+		String port = String.valueOf(getAvailPort());
+		StringBuilder cmd = new StringBuilder("java -agentlib:jdwp=transport=dt_socket,address="
+			+port+",server=y,suspend=y");
+		cmd.append(" -cp " + getClassPath(classPathXml));
+		cmd.append(" ");
+		cmd.append(mainClass);
+		
+		String projectRoot = new File(classPathXml).getParent();
+
+		try {
+			File workingDir = new File(projectRoot);
+			process = Runtime.getRuntime().exec(cmd.toString(),null,workingDir);
+		} catch (Exception err) {
+			err.printStackTrace();
+		}
+		for (int i=0; i<10; i++) {
+			VirtualMachine vm = attachToVm(port); 
+			if (vm != null) return vm;
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {}
+		}
+		return null;
+	}
+
+	public VirtualMachine launch2(String mainClass, String classPathXml,
+			List<String> opts,List<String> args) {
+		LaunchingConnector launchingConnector = Bootstrap
+				.virtualMachineManager().defaultConnector();
+
+		Map<String, Connector.Argument> defaultArguments = launchingConnector
+				.defaultArguments();
+
+		Connector.Argument mainArg = defaultArguments.get("main");
+		StringBuilder mainSb = new StringBuilder(mainClass);
+		
+		if (args != null & args.size() > 0 ) {
+			mainSb.append(" ");
+			for (String arg : args) {
+				mainSb.append(arg).append(" ");
+			}
+		}
+		mainArg.setValue(mainSb.toString());
+
+		Connector.Argument suspendArg = defaultArguments.get("suspend");
+		suspendArg.setValue("true");
+
+		Connector.Argument optionArg = (Connector.Argument) defaultArguments
+				.get("options");
+		
+		StringBuilder optionSb = new StringBuilder();
+		String projectRoot = new File(classPathXml).getParent();
+		String user_dir = "-Duser.dir=" + projectRoot ;
+		
+		String cp = "-Djava.class.path=" + getClassPath(classPathXml);
+		optionSb.append(user_dir).append(" ");
+		optionSb.append(cp).append(" ");
+		if (opts != null) {
+			for (String opt : opts) {
+				optionSb.append(opt).append(" ");
+			}
+		}
+		optionArg.setValue(optionSb.toString());
+		
+
+		try {
+			VirtualMachine vm = launchingConnector.launch(defaultArguments);
+			return vm;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IllegalConnectorArgumentsException e) {
+			e.printStackTrace();
+		} catch (VMStartException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public VirtualMachine attachToVm(String port) {
+		AttachingConnector connector = getAttachingConnector();
+		if (connector == null) return null;
+		Map<String, Connector.Argument> args = connector.defaultArguments();
+		Connector.Argument portArg = args.get("port");
+		Connector.Argument hostArg = args.get("hostname");
+		try {
+			portArg.setValue(port);
+			hostArg.setValue("127.0.0.1"); 
+			VirtualMachine vm = connector.attach(args);
+			return vm;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private AttachingConnector getAttachingConnector() {
+		VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
+		List<AttachingConnector> connectors = vmm.attachingConnectors();
+		AttachingConnector connector = null;
+		for (AttachingConnector conn : connectors) {
+			if (conn.name().equals("com.sun.jdi.SocketAttach"))
+				connector = conn;
+		}
+		return connector;
 	}
 }
 	 
